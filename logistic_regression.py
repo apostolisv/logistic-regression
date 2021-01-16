14bf9fc0c5a9f63d85c352bd994d2a5f40dfc5c1
Apostolis V <41242107+apostolisv@users.noreply.github.com>
2021-01-16 17:54:35 +0200

    Add project files.

---
+++ logistic_regression.py
@@ -0,0 +1,38 @@
+#Binomial logistic regression
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from LoadFile import LoadFile
+
+url = 'C:\\Users\\Apostolis\\Desktop\\aiHW\\aclImdb'
+urltest = url + '\\test'
+urltrain = url + '\\train'
+
+
+def read_train():
+    pass
+
+
+def read_test():
+    pass
+
+
+def train():
+    pass
+
+
+def test():
+    pass
+
+def cost():
+    pass
+
+def logistic_regression():
+    pass
+
+
+
+load = LoadFile()
+f = []
+load.createDictionary()
